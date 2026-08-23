baba525be1aca6b1dbf0ecf7a6e93ddbdf0825ba
snehayadav278 <snehay851@gmail.com>
2026-08-23 11:26:52 +0530

    learned css

---
+++ 05-css/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header/Header.jsx";
+import Button from "./components/button/Button.jsx";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Button />
+    </div>
+  )
+}
+
+export default App;
---
+++ 05-css/src/components/button/Button.jsx
@@ -0,0 +1,11 @@
+import styles from './Button.module.css'
+
+const Button = () => {
+    return(
+        <div className={styles.btn}>
+            this is button
+        </div>
+    )
+}
+
+export default Button;
